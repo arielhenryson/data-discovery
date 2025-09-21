9876b1930d810392f025be707f184981db4a2203
ariel.henryson <ariel.henryson@gmail.com>
2025-09-21 13:41:48 +0300

    wip

--- app/src/app/pages/blank/login/login.component.ts
+++ app/src/app/pages/blank/login/login.component.ts
@@ -1,10 +1,21 @@
 import { Component, ChangeDetectionStrategy, inject } from '@angular/core'
 import { Router } from '@angular/router'
+import { UserStore } from '../../../store/user.store'
 
 @Component({
   templateUrl: 'login.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent {
+  userStore = inject(UserStore)
+
   router = inject(Router)
+
+  async login() {
+    const success = await this.userStore.signIn()
+
+    if (success) {
+      this.router.navigate([ '/' ])
+    }
+  }
 }
---
+++ app/src/app/store/user.store.ts
@@ -0,0 +1,46 @@
+// src/app/store/user.store.ts
+import { signalStore, withMethods, withState, patchState } from '@ngrx/signals'
+
+/**
+ * Interface representing a user.
+ */
+interface User {
+  id: string;
+  email: string;
+  name: string;
+}
+
+/**
+ * Interface for the entire user state, including user info and settings.
+ */
+interface UserState {
+  user: User | undefined;
+}
+
+/**
+ * The initial state for the user store.
+ */
+const initialState: UserState = {
+  user: undefined,
+}
+
+export const UserStore = signalStore(
+  { providedIn: 'root' },
+  withState(initialState),
+  withMethods((
+    store, 
+  ) => ({
+    async logout() {
+      console.log('logout')
+      patchState(store, { user: undefined })
+    },
+
+    async signIn(): Promise<boolean> {
+      console.log('signIn')
+
+      patchState(store, { user: undefined })
+
+      return true
+    },
+  })),
+)
