5eeb60e62882a25c23bd1176b95fe88ac445763f
ariel.henryson <ariel.henryson@gmail.com>
2025-09-21 12:06:32 +0300

    wip

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,10 +1,21 @@
 import { Routes } from '@angular/router'
-
 import { LoginComponent } from './pages/blank/login/login.component'
+import { MainComponent } from './pages/main/main.component'
+import { HomeComponent } from './pages/main/home/home.component'
 
 export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+    ],
   },
 ]
